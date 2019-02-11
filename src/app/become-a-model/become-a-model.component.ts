579880494453da78f6501e6945c87ec6fbe31920
koushikbiswas <biswakoushik.influxiq@gmail.com>
2019-02-11 12:23:39 +0530

    first commit

---
+++ src/app/become-a-model/become-a-model.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-become-a-model',
+  templateUrl: './become-a-model.component.html',
+  styleUrls: ['./become-a-model.component.css']
+})
+export class BecomeAModelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/model-profile/model-profile.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-model-profile',
+  templateUrl: './model-profile.component.html',
+  styleUrls: ['./model-profile.component.css']
+})
+export class ModelProfileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+
+  }
+
+}
+
+
